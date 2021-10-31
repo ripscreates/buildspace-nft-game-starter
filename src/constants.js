a43f81bcc0cb9f1f1f9b7cfdf32978ceb41ed253
Ripley <4315909+ripscreates@users.noreply.github.com>
2021-10-30 22:46:29 -0700

    Added constants.js
    updated to reflect changes for my project.

---
+++ src/constants.js
@@ -0,0 +1,16 @@
+const CONTRACT_ADDRESS = '0xC3d55FDaF23B338f1a098c19B7ba371cfcF49Bc2';
+
+/*
+ * Add this method and make sure to export it on the bottom!
+ */
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData };
